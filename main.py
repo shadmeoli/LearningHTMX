8e42da3e2922a9af8a5f59b21de645a65f16a8ae
shadmeoli <shadcodes@gmail.com>
2023-07-26 11:23:15 +0300

    updates: HTMX CDN features

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.exceptions import HTTPException
+from htmx import hx
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="src/template")
@@ -13,3 +15,10 @@ def home(request: Request):
     }
     return templates.TemplateResponse('index.html', packet)
 
+
+@app.route('/togglePassword', methods=['GET'])
+@hx.vars
+def toggle_password():
+    password_type = request.args.get('password_type', 'password')
+    new_password_type = 'text' if password_type == 'password' else 'password'
+    return jsonify({'password_type': new_password_type})
